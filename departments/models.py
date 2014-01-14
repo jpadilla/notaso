ef925877bd9d03fa5c0bbb09d44656fd64bb85a6
eluciano11 <emma.luciano11@gmail.com>
2014-01-13 20:34:16 -0400

    correct departments

--- departments/models.py
+++ departments/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 # Create your models here.
-class Departments(models.Model):
-	department_name = models.CharField(max_length=50)
+class Department(models.Model):
+	department_name = models.CharField(max_length=50, unique = True)
 
 	def __unicode__(self):
 		return self.department_name 
