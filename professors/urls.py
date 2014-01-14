0166d81a8a49bba581e91d66e6bc8a3926de44ee
eluciano11 <emma.luciano11@gmail.com>
2014-01-13 20:30:36 -0400

    professors app changes

--- professors/urls.py
+++ professors/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
 	url(r'^(?P<professor_id>\d+)/$', 'professors.views.specific_professor_view', name = 'specified_professor'),
+	url(r'^create/$', 'professors.views.create_professor_view', name = 'create_professor'),
 )
