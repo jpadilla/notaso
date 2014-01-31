49338a5ef9bca76590d445ce5a8c921b42bc989e
eluciano11 <emma.luciano11@gmail.com>
2014-01-31 00:28:56 -0400

    comment, department, professors and universities finished

--- comments/forms.py
+++ comments/forms.py
@@ -3,23 +3,22 @@ import datetime
 from django import forms
 from django.shortcuts import get_object_or_404
 
-from users.models import User
 from professors.models import Professor
 from .models import Comments
 
 
 class AddCommentForm(forms.ModelForm):
-	responsibility = forms.IntegerField(required=False)
-	personality = forms.IntegerField(required=False)
-	workload = forms.IntegerField(required=False)
-	dificulty = forms.IntegerField(required=False)
-	class Meta:
-		model = Comments
+    responsibility = forms.IntegerField(required=False)
+    personality = forms.IntegerField(required=False)
+    workload = forms.IntegerField(required=False)
+    dificulty = forms.IntegerField(required=False)
+    class Meta:
+        model = Comments
         fields = ['body', 'is_anonymous', 'responsibility', 'personality', 'workload', 'dificulty']
 
-	def save_form(self, user, prof_id):
-		c = self.save(commit=False)
-		c.created_by = get_object_or_404(User, email=user)
-		c.professor = get_object_or_404(Professor, pk=prof_id)
-		c.date = datetime.datetime.today()
-		c.save()
+    def save_form(self, request, prof_id):
+        c = self.save(commit=False)
+        c.created_by = request.user
+        c.professor = get_object_or_404(Professor, pk=prof_id)
+        c.created_at = datetime.datetime.today()
+        c.save()
