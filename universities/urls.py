8c7bd21d756bca5b479824bbf51dd1028585a768
eluciano11 <emma.luciano11@gmail.com>
2014-02-15 23:50:23 -0400

    get methods for professors model done

--- universities/urls.py
+++ universities/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 
-urlpatterns = patterns('',
+urlpatterns = patterns(
+    '',
     url(r'^$', 'universities.views.universities_view', name='universities'),
     url(r'^(?P<slug>[-_\w]+)/$', 'universities.views.specific_university_view', name='specified_university'),
     url(r'^(?P<slug>[-_\w]+)/', include('departments.urls'), name='departments'),
