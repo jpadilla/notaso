69aa8157556b7995815cf93461f1a4539cd800d9
eluciano11 <emma.luciano11@gmail.com>
2014-03-18 13:26:40 -0400

    create_professors done, specific_professors in process

--- professors/forms.py
+++ professors/forms.py
@@ -7,7 +7,13 @@ class AddProfessorForm(forms.ModelForm):
     class Meta:
         model = Professor
         fields = ['first_name', 'last_name', 'gender', 'university', 'department']
-
+        widgets = {
+            'first_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'last_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'gender': forms.Select(attrs={'class': 'form-control'}),
+            'university': forms.Select(attrs={'class': 'form-control'}),
+            'department': forms.Select(attrs={'class': 'form-control'}),
+        }
     def save_form(self, request):
         p = self.save(commit=False)
         p.created_by = request.user
