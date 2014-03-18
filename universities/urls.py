364d415b8987abc885bf3592c107df59e810d1a5
jose <jpadilla1293@gmail.com>
2014-03-18 18:06:20 -0400

    Created department template, query count problem in universities page (212 queries)

--- universities/urls.py
+++ universities/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import patterns, include, url
 
+from . import views
+
 urlpatterns = patterns(
     '',
-    url(r'^$', 'universities.views.universities_view', name='universities'),
-    url(r'^(?P<slug>[-_\w]+)/$', 'universities.views.specific_university_view', name='specified_university'),
+    url(r'^$', views.universities_view, name='universities'),
+    url(r'^(?P<slug>[-_\w]+)/$', views.specific_university_view, name='specified_university'),
     url(r'^(?P<slug>[-_\w]+)/', include('departments.urls', namespace='departments')),
 )
