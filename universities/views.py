e7f0cb193198a6e177a75a45c8150b70af6b0ed5
José Padilla <jpadilla@webapplicate.com>
2014-03-21 10:02:18 -0400

    Reduced query counts in universities view

--- universities/views.py
+++ universities/views.py
@@ -21,17 +21,21 @@ def universities_view(request):
 
 def specific_university_view(request, slug):
     university = get_object_or_404(University, slug=slug)
+
+    professors = Professor.objects.filter(
+        university=university).select_related('university')
+
+    comments = Comment.objects.filter(
+        professor__in=professors).select_related(
+        'created_by').exclude(body__exact='')
+
     data = {
         'specified_university': university,
         'departments': Department.objects.all(),
-        'hi_professors': Professor.objects.filter(
-            university=university).order_by('-score')[:5],
+        'hi_professors': professors.order_by('-score')[:5],
         'grade': university.get_grade(),
-        'low_professors': Professor.objects.filter(
-            university=university).order_by('score')[:5],
-        'recent_comments': Comment.objects.filter(
-            professor__in=Professor.objects.filter(
-                university=university)).exclude(
-        body__exact='').order_by('-created_at')[:5]
+        'low_professors': professors.order_by('score')[:5],
+        'recent_comments': comments.order_by('-created_at')[:5]
     }
+
     return render_to_response("university.html", data)
