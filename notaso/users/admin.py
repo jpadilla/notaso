0825479f65b3775a47afa8f6c17196583649458f
jose <jpadilla1293@gmail.com>
2014-03-29 13:44:38 -0400

    fixed users

--- notaso/users/admin.py
+++ notaso/users/admin.py
@@ -14,19 +14,19 @@ class MyUserAdmin(UserAdmin):
     # The fields to be used in displaying the User model.
     # These override the definitions on the base UserAdmin
     # that reference specific fields on auth.User.
-    list_display = ('email', 'first_name', 'last_name', 'is_admin')
-    list_filter = ('is_admin',)
+    list_display = ('email', 'first_name', 'last_name', 'is_active', 'is_admin')
+    list_filter = ('is_admin', 'is_active')
     fieldsets = (
         (None, {'fields': ('email', 'password')}),
         ('Personal info', {'fields': ('first_name', 'last_name', )}),
-        ('Permissions', {'fields': ('is_admin',)}),
+        ('Permissions', {'fields': ('is_admin', 'is_active')}),
     )
     # add_fieldsets is not a standard ModelAdmin attribute. UserAdmin
     # overrides get_fieldsets to use this attribute when creating a user.
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('email', 'first_name', 'last_name', 'password1', 'password2')}
+            'fields': ('email', 'first_name', 'last_name', 'password1', 'password2', 'is_active')}
         ),
     )
     search_fields = ('email',)
--- notaso/users/views.py
+++ notaso/users/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import redirect
-from users.admin import UserCreationForm
-from .forms import UserLoginForm
+from django.shortcuts import redirect, render
+from django.contrib import auth
+
+from .forms import UserLoginForm, UserCreationForm
 
 
 def register(request):
@@ -13,3 +14,26 @@ def register(request):
         return redirect('/')
 
     form = UserLoginForm(request.POST or None)
+
+
+def login(request):
+    if request.user.is_authenticated():
+        return redirect('/')
+
+    form = UserLoginForm(request.POST or None)
+    if form.is_valid():
+        email = form.cleaned_data['email']
+        password = form.cleaned_data['password']
+        user = auth.authenticate(username=email, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+
+    return render(request, 'users/login.html', {'form': form})
+
+
+def logout(request):
+    if request.user.is_authenticated():
+        auth.logout(request)
+        return redirect('/')
