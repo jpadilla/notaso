8319c0400d2a4f9c0bdd119be9f5cdc305ac88ff
jose <jpadilla1293@gmail.com>
2014-03-29 13:03:46 -0400

    attempt to fix users

--- notaso/users/models.py
+++ notaso/users/models.py
@@ -1,10 +1,44 @@
-from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
 
 
-class User(AbstractUser):
+class MyUserManager(BaseUserManager):
+    def create_user(self, email, first_name, last_name, password=None):
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        user = self.model(
+            email=self.normalize_email(email),
+            first_name=first_name,
+            last_name=last_name,
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, first_name, last_name, password):
+        user = self.create_user(
+            email,
+            password=password,
+            first_name=first_name,
+            last_name=last_name,
+        )
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser):
+    email = models.EmailField(max_length=40, unique=True)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    is_admin = models.BooleanField(default=False)
+
+    objects = MyUserManager()
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username']
+    REQUIRED_FIELDS = ['first_name', 'last_name']
 
     def get_full_name(self):
         return "%s %s" % (self.first_name, self.last_name)
@@ -13,9 +47,20 @@ class User(AbstractUser):
         return self.first_name
 
     def __unicode__(self):
-        return self.username
+        return self.email
 
-User._meta.get_field_by_name('username')[0]._unique = False
-User._meta.get_field_by_name('username')[0]._blank = True
-User._meta.get_field_by_name('email')[0]._blank = False
-User._meta.get_field_by_name('email')[0]._unique = True
+    def has_perm(self, perm, obj=None):
+        "Does the user have a specific permission?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    def has_module_perms(self, app_label):
+        "Does the user have permissions to view the app `app_label`?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    @property
+    def is_staff(self):
+        "Is the user a member of staff?"
+        # Simplest possible answer: All admins are staff
+        return self.is_admin
--- notaso/users/views.py
+++ notaso/users/views.py
@@ -1,42 +1,15 @@
-from django.shortcuts import render
-from django.http import HttpResponseRedirect
-from django.contrib import auth
-from django.core.urlresolvers import reverse
-
-from .forms import LoginForm, SignupForm
+from django.shortcuts import redirect
+from users.admin import UserCreationForm
+from .forms import UserLoginForm
 
 
 def register(request):
-    form = SignupForm(request.POST or None)
+    form = UserCreationForm(request.POST or None)
 
     if form.is_valid():
         form.save()
 
-        return HttpResponseRedirect(
-            reverse('users:login')
-        )
-    return render(request, 'users/signup.html', {'form': form})
-
-
-def login(request):
     if request.user.is_authenticated():
-        return HttpResponseRedirect('/')
+        return redirect('/')
 
-    form = LoginForm(request.POST or None)
-    if form.is_valid():
-        email = form.cleaned_data['email']
-        password = form.cleaned_data['password']
-        user = auth.authenticate(username=email, password=password)
-
-        if user is not None:
-            auth.login(request, user)
-            return HttpResponseRedirect('/')
-
-    return render(request, 'users/login.html', {'form': form})
-
-
-def logout(request):
-    if request.user.is_authenticated():
-        auth.logout(request)
-
-    return HttpResponseRedirect('/')
+    form = UserLoginForm(request.POST or None)
