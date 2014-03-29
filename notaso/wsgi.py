888da18cb6957802baca23ff342e19493791c63f
José Padilla <jpadilla@webapplicate.com>
2014-03-29 10:43:41 -0400

    Install and setup Cling

--- notaso/wsgi.py
+++ notaso/wsgi.py
@@ -16,4 +16,6 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'notaso.settings')
 os.environ.setdefault('DJANGO_CONFIGURATION', ENVIRONMENT)
 
 from configurations.wsgi import get_wsgi_application
-application = get_wsgi_application()
+from dj_static import Cling
+
+application = Cling(get_wsgi_application())
