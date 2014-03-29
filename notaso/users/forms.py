c990afe3ad096e01c8695cfb3b03c5778aa55afb
jose <jpadilla1293@gmail.com>
2014-03-29 13:07:37 -0400

    attempt to fix users #2

--- notaso/users/forms.py
+++ notaso/users/forms.py
@@ -40,7 +40,7 @@ class UserChangeForm(forms.ModelForm):
 
     class Meta:
         model = User
-        fields = ['email', 'password', 'first_name', 'last_name', 'is_active', 'is_admin']
+        fields = ['email', 'password', 'first_name', 'last_name', 'is_admin']
 
     def clean_password(self):
         # Regardless of what the user provides, return the initial value.
