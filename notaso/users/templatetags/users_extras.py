0dff50413ee309f4d8e73b27bd1a0ec2ef114297
José Padilla <jpadilla@webapplicate.com>
2014-03-29 12:20:55 -0400

    Fixes for https assets

--- notaso/users/templatetags/users_extras.py
+++ notaso/users/templatetags/users_extras.py
@@ -2,12 +2,16 @@ import urllib
 import hashlib
 
 from django import template
+from django.conf import settings
+
 register = template.Library()
 
 
 @register.filter(name='gravatar_url')
 def gravatar_url(instance, email):
     size = 40
-    gravatar_url = "http://www.gravatar.com/avatar/"+ hashlib.md5(email.lower()).hexdigest() + "?"
-    gravatar_url += urllib.urlencode({'d': "retro", 's': str(size)})
-    return gravatar_url
+    hash = hashlib.md5(email.lower()).hexdigest()
+    params = urllib.urlencode({'d': "retro", 's': str(size)})
+
+    return "{}://www.gravatar.com/avatar/{}?".format(
+        settings.PROTOCOL, hash, params)
