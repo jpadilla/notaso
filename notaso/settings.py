8ea6a35524c8fa674ee9a4d64ffa8be251bad59d
José Padilla <jpadilla@webapplicate.com>
2014-03-29 11:25:17 -0400

    Fix STATIC_ROOT error

--- notaso/settings.py
+++ notaso/settings.py
@@ -80,9 +80,8 @@ class Common(Configuration):
 
     # Static files (CSS, JavaScript, Images)
     # https://docs.djangoproject.com/en/1.6/howto/static-files/
+    STATIC_ROOT = 'assets'
     STATIC_URL = '/static/'
-
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
     STATICFILES_DIRS = (
         ('static', os.path.join(BASE_DIR, 'static')),
