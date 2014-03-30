8c1a41bd7eecb45d84b83131051b45eaf6c0e079
Jpadilla1 <jpadilla1293@gmail.com>
2014-03-30 19:05:17 -0400

    fixed users in admin

--- notaso/users/admin.py
+++ notaso/users/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.models import Group
 from django.contrib.auth.admin import UserAdmin
-from django.contrib.auth.forms import UserChangeForm, UserCreationForm
+from .forms import UserChangeForm, UserCreationForm
 
 from .models import User
 
