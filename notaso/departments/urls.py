0e4421999d4434f7b62bc57ee5c404664d2ae1de
José Padilla <jpadilla@webapplicate.com>
2014-03-29 22:01:11 -0400

    Fix weird import loop error

--- notaso/departments/urls.py
+++ notaso/departments/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import patterns, url
 
+from .views import department_view
+
 urlpatterns = patterns(
     '',
+
     url(r'^(?P<department_slug>[-_\w]+)/$',
-    'departments.views.department_view', name="specified_department"),
+        department_view, name="specified_department"),
 )
