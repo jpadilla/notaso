4bdc54f1ede8dfbdc08dd3e3e6c8fa5791fbc472
Jpadilla1 <jpadilla1293@gmail.com>
2014-04-06 23:34:08 -0400

    attempt to fix graph issue

--- notaso/professors/templatetags/graph_creation.py
+++ notaso/professors/templatetags/graph_creation.py
@@ -1,29 +1,21 @@
-import urllib
-import hashlib
-
 from django import template
-from django.conf import settings
 from GChartWrapper import *
-# http://code.google.com/apis/chart/#sparkline  
+# http://code.google.com/apis/chart/#sparkline
 
 register = template.Library()
+
+
 @register.simple_tag
 def graph(comments):
     scores = []
-    sum_responsibility = 0
-    sum_personality = 0
-    sum_workload = 0
-    sum_difficulty = 0
-    count_comment = comments.count()
     for c in comments:
         scores.append(float(c.responsibility-1+c.personality-1 +
-                       c.workload-1+c.difficulty-1)*10)
+                            c.workload-1+c.difficulty-1)*10)
     chart = Sparkline(scores)
     chart.color('0077CC')
-    chart.size(450,250)
-    chart.marker('B', 'E6F2FA',0,0,0)
-    chart.line(1,0,0)
+    chart.size(450, 250)
+    chart.marker('B', 'E6F2FA', 0, 0, 0)
+    chart.line(1, 0, 0)
     chart.axes('y')
-    chart.axes.range(0,10,100,10)
+    chart.axes.range(0, 10, 100, 10)
     return chart.img()
-    
