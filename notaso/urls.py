2d8b0968439971d6c0c667743b3c4dc0ea23e64c
jose <jpadilla1293@gmail.com>
2014-04-08 19:35:24 -0400

    added legal docs

--- notaso/urls.py
+++ notaso/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic import TemplateView
 
 from django.contrib import admin
 
@@ -24,4 +25,9 @@ urlpatterns = patterns(
         include('notaso.professors.urls', namespace='professors')),
 
     url(r'^search/', include('notaso.search.urls', namespace='search')),
+
+    # Static pages
+
+    url(r'^legal/$', TemplateView.as_view(
+        template_name='static/legal.html'), name='legal')
 )
