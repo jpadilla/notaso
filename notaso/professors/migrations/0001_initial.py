2f474dc3c7d2b31fa033b09d6e8fddecad6f5bb1
Jose E Padilla <jpadilla1293@gmail.com>
2014-05-07 12:27:29 -0400

    added and implemented djorm-ext-pgfulltext

---
+++ notaso/professors/migrations/0001_initial.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'Professor'
+        db.create_table(u'professors_professor', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('first_name', self.gf('django.db.models.fields.CharField')(max_length=25)),
+            ('last_name', self.gf('django.db.models.fields.CharField')(max_length=75)),
+            ('gender', self.gf('django.db.models.fields.CharField')(max_length=1)),
+            ('university', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['universities.University'])),
+            ('department', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['departments.Department'])),
+            ('created_by', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['users.User'])),
+            ('slug', self.gf('autoslug.fields.AutoSlugField')(unique=True, max_length=50, populate_from=None, unique_with=())),
+            ('score', self.gf('django.db.models.fields.FloatField')(default=0)),
+            ('responsibility', self.gf('django.db.models.fields.FloatField')(default=0)),
+            ('personality', self.gf('django.db.models.fields.FloatField')(default=0)),
+            ('workload', self.gf('django.db.models.fields.FloatField')(default=0)),
+            ('difficulty', self.gf('django.db.models.fields.FloatField')(default=0)),
+        ))
+        db.send_create_signal(u'professors', ['Professor'])
+
+
+    def backwards(self, orm):
+        # Deleting model 'Professor'
+        db.delete_table(u'professors_professor')
+
+
+    models = {
+        u'departments.department': {
+            'Meta': {'object_name': 'Department'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '30'}),
+            'slug': ('autoslug.fields.AutoSlugField', [], {'unique': 'True', 'max_length': '50', 'populate_from': "'name'", 'unique_with': '()'})
+        },
+        u'professors.professor': {
+            'Meta': {'object_name': 'Professor'},
+            'created_by': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['users.User']"}),
+            'department': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['departments.Department']"}),
+            'difficulty': ('django.db.models.fields.FloatField', [], {'default': '0'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '25'}),
+            'gender': ('django.db.models.fields.CharField', [], {'max_length': '1'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '75'}),
+            'personality': ('django.db.models.fields.FloatField', [], {'default': '0'}),
+            'responsibility': ('django.db.models.fields.FloatField', [], {'default': '0'}),
+            'score': ('django.db.models.fields.FloatField', [], {'default': '0'}),
+            'slug': ('autoslug.fields.AutoSlugField', [], {'unique': 'True', 'max_length': '50', 'populate_from': 'None', 'unique_with': '()'}),
+            'university': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['universities.University']"}),
+            'workload': ('django.db.models.fields.FloatField', [], {'default': '0'})
+        },
+        u'universities.university': {
+            'Meta': {'object_name': 'University'},
+            'city': ('django.db.models.fields.CharField', [], {'max_length': '25'}),
+            'emblem': ('django.db.models.fields.files.FileField', [], {'max_length': '100'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'max_length': '50'})
+        },
+        u'users.user': {
+            'Meta': {'object_name': 'User'},
+            'email': ('django.db.models.fields.EmailField', [], {'unique': 'True', 'max_length': '254'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_admin': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'})
+        }
+    }
+
+    complete_apps = ['professors']
