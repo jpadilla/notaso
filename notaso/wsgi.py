94633739de39c110503851114d98750ee720a529
José Padilla <jpadilla@webapplicate.com>
2015-09-15 16:49:44 -0400

    Load dotenv in wsgi

--- notaso/wsgi.py
+++ notaso/wsgi.py
@@ -6,8 +6,15 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/dev/howto/deployment/wsgi/
 """
-
 import os
+import dotenv
+
+
+try:
+    dotenv.read_dotenv(
+        os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env'))
+except Exception as e:
+    print(e)
 
 
 ENVIRONMENT = os.getenv('ENVIRONMENT', 'DEVELOPMENT').title()
