1fc98c99110ec78e45188745ba850a833d817fde
José Padilla <jpadilla@webapplicate.com>
2017-07-17 07:48:50 -0400

    Autodiscover apps for admin

--- notaso/urls.py
+++ notaso/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import include, url
 from django.views.generic import TemplateView
 from django.contrib import admin
 
+admin.autodiscover()
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
