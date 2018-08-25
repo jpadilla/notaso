b8610f46f7ec21f8da3f033abdaff0af988a6469
José Padilla <jpadilla@webapplicate.com>
2018-08-25 10:00:16 -0400

    Fix wsgi import order

--- notaso/wsgi.py
+++ notaso/wsgi.py
@@ -1,6 +1,5 @@
 import os
 
-from configurations.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
 ENVIRONMENT = os.getenv("ENVIRONMENT", "DEVELOPMENT").title()
@@ -8,6 +7,7 @@ ENVIRONMENT = os.getenv("ENVIRONMENT", "DEVELOPMENT").title()
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "notaso.settings")
 os.environ.setdefault("DJANGO_CONFIGURATION", ENVIRONMENT)
 
+from configurations.wsgi import get_wsgi_application  # noqa isort:skip
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
