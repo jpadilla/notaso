babb92619c0beb24cca1bf69bfd3bb9cbf373aa5
Raúl Negrón <raul.esteban.negron@gmail.com>
2018-09-02 22:47:03 -0400

    Update migration file to reflect model field name changes

--- notaso/users/migrations/0002_auto_20180826_1446.py
+++ notaso/users/migrations/0002_auto_20180826_1446.py
@@ -15,20 +15,20 @@ class Migration(migrations.Migration):
     operations = [
         migrations.AddField(
             model_name="user",
-            name="created",
+            name="created_at",
             field=model_utils.fields.AutoCreatedField(
                 default=django.utils.timezone.now,
                 editable=False,
-                verbose_name="created",
+                verbose_name="created_at",
             ),
         ),
         migrations.AddField(
             model_name="user",
-            name="modified",
+            name="modified_at",
             field=model_utils.fields.AutoLastModifiedField(
                 default=django.utils.timezone.now,
                 editable=False,
-                verbose_name="modified",
+                verbose_name="modified_at",
             ),
         ),
     ]
